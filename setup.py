7a98175b65c4444645e328ed6738577b4b715a18
Abram Symons <abramsymons@gmail.com>
2021-01-12 22:02:55 +0330

    use brightid instead of brightid-cli for cli command

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brightidcli',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -14,9 +14,10 @@ setup(
         'urllib3==1.26.2',
         'python-arango==5.4.0',
         'ed25519==1.5',
+        'python-brightid'
     ],
     entry_points='''
         [console_scripts]
-        brightid-cli=brightidcli.main:cli
+        brightid=brightidcli.main:cli
     ''',
 )
